ea80483a429d1c8f248bf6595fc4dd5df1463ac2
Vladimir <besshaposhnikovv@gmail.com>
2026-03-31 17:42:19 +0300

    chore: initial project setup

---
+++ src/main/java/ru/anoddyne/temperaturesensorserver/TemperatureSensorServerApplication.java
@@ -0,0 +1,13 @@
+package ru.anoddyne.temperaturesensorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureSensorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureSensorServerApplication.class, args);
+    }
+
+}
